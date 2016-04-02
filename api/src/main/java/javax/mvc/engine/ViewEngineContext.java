5189b91c77cded0e39eaa25127022aaa31301bed
Ivar Grimstad <ivar.grimstad@gmail.com>
2016-04-02 08:34:24 +0200

    Remove Servlet dependencies from context
    HttpServletRequest and HttpServletResponse removed from ViewEngineContext.

--- api/src/main/java/javax/mvc/engine/ViewEngineContext.java
+++ api/src/main/java/javax/mvc/engine/ViewEngineContext.java
@@ -40,8 +40,6 @@
 package javax.mvc.engine;
 
 import javax.mvc.Models;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Configuration;
 import javax.ws.rs.core.UriInfo;
@@ -52,9 +50,8 @@ import javax.ws.rs.core.UriInfo;
  * objects from the Servlet container, among other data.</p>
  *
  * @author Santiago Pericas-Geertsen
+ * @author Ivar Grimstad
  * @see javax.mvc.engine.ViewEngine
- * @see javax.servlet.http.HttpServletRequest
- * @see javax.servlet.http.HttpServletResponse
  * @since 1.0
  */
 public interface ViewEngineContext {
@@ -72,21 +69,6 @@ public interface ViewEngineContext {
      * @return the models instance.
      */
     Models getModels();
-
-    /**
-     * Returns HTTP request object from the Servlet container.
-     *
-     * @return HTTP request object.
-     */
-    HttpServletRequest getRequest();
-
-    /**
-     * Returns HTTP response object from the servlet container. The underlying
-     * output stream should be used to write the result of processing a view.
-     *
-     * @return HTTP response object.
-     */
-    HttpServletResponse getResponse();
 
     /**
      * Returns the {@link javax.ws.rs.core.UriInfo} instance containing information
