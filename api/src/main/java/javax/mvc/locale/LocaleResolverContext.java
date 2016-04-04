b5dd4cca62eafc0de75da2ff45b1af48531c26fb
Christian Kaltepoth <christian@kaltepoth.de>
2016-04-04 17:33:14 +0200

    MVC_SPEC-27 Refactored LocaleResolverContext

--- api/src/main/java/javax/mvc/locale/LocaleResolverContext.java
+++ api/src/main/java/javax/mvc/locale/LocaleResolverContext.java
@@ -39,35 +39,73 @@
  */
 package javax.mvc.locale;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Configuration;
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * <p>Contextual data used by a {@link javax.mvc.locale.LocaleResolver} to resolve the request
- * locale. This includes the request from the Servlet container, the JAX-RS configuration,
- * among other data.</p>
+ * locale.</p>
  *
  * @author Christian Kaltepoth
  * @see javax.mvc.locale.LocaleResolver
- * @see javax.servlet.http.HttpServletRequest
  * @since 1.0
  */
 public interface LocaleResolverContext {
 
     /**
-     * Returns HTTP request object from the Servlet container.
-     *
-     * @return HTTP request object.
-     */
-    HttpServletRequest getRequest();
-
-    /**
      * Returns the application's configuration.
      *
      * @return application's configuration.
-     * @see javax.servlet.http.HttpServletRequest
      * @see javax.ws.rs.core.Configuration
      */
     Configuration getConfiguration();
+
+    /**
+     * Get a list of languages that are acceptable for the response according to the
+     * <code>Accept-Language</code> HTTP header sent by the client.
+     *
+     * @return a read-only list of languages ordered by their q-value.
+     * @see Locale
+     */
+    List<Locale> getAcceptableLanguages();
+
+    /**
+     * Get the JAX-RS {@link Request} instance.
+     *
+     * @return the JAX-RS {@link Request} instance.
+     * @see Request
+     */
+    Request getRequest();
+
+    /**
+     * Get request URI information.
+     *
+     * @return request URI information
+     * @see UriInfo
+     */
+    UriInfo getUriInfo();
+
+    /**
+     * Returns the cookie with the given name.
+     *
+     * @param name the name of the cookie
+     * @return the cookie or <code>null</code> if no cookie with this name exists
+     * @see Cookie
+     */
+    Cookie getCookie(String name);
+
+    /**
+     * Get the request header as a single string value.
+     *
+     * @param name the name of the request header
+     * @return the request header value. If the request header is not present then
+     * {@code null} is returned. If the request header is present more than once then the values
+     * of joined together and separated by a ',' character.
+     */
+    String getHeaderString(String name);
 
 }
