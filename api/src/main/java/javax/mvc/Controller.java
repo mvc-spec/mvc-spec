0f4f2fb422e1a550c12e5087805e507e9a4290e9
Christian Kaltepoth <christian@kaltepoth.de>
2018-06-10 11:42:03 +0200

    Moved View annotation to javax.mvc package

--- api/src/main/java/javax/mvc/Controller.java
+++ api/src/main/java/javax/mvc/Controller.java
@@ -29,9 +29,9 @@ import static java.lang.annotation.ElementType.*;
  * it applies to all methods in the type.</p>
  *
  * <p>A controller method that returns void is required to be annotated with
- * {@link javax.mvc.annotation.View}. A controller method can return a view path
+ * {@link View}. A controller method can return a view path
  * as a {@link java.lang.String} or a {@link javax.ws.rs.core.Response}. If a controller
- * returns {@code null} and it is annotated with {@link javax.mvc.annotation.View},
+ * returns {@code null} and it is annotated with {@link View},
  * then the value of this annotation is used as a default.</p>
  *
  * <p>Example:
@@ -43,7 +43,7 @@ import static java.lang.annotation.ElementType.*;
  * </code></pre>
  *
  * @author Santiago Pericas-Geertsen
- * @see javax.mvc.annotation.View
+ * @see View
  * @see javax.ws.rs.core.Response
  * @since 1.0
  */
--- api/src/main/java/javax/mvc/View.java
+++ api/src/main/java/javax/mvc/View.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package javax.mvc.annotation;
+package javax.mvc;
 
 import javax.mvc.Controller;
 import java.lang.annotation.Documented;
