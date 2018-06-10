f178f1a5b951077cea7b2d61f27124f9f2ec80a7
Christian Kaltepoth <christian@kaltepoth.de>
2018-06-10 11:45:11 +0200

    Moved MvcUriBuilder class to javax.mvc.uri package

--- api/src/main/java/javax/mvc/uri/MvcUriBuilder.java
+++ api/src/main/java/javax/mvc/uri/MvcUriBuilder.java
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package javax.mvc;
+package javax.mvc.uri;
 
+import javax.mvc.MvcContext;
 import java.net.URI;
 import java.util.Map;
 
