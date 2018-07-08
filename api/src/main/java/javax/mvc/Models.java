695e32317ba8d91aae044a27e1e8f0d86ca39ec7
Christian Kaltepoth <christian@kaltepoth.de>
2018-07-08 07:29:54 +0200

    Modified Models interface to not extend Map (#40) (#171)

--- api/src/main/java/javax/mvc/Models.java
+++ api/src/main/java/javax/mvc/Models.java
@@ -27,9 +27,45 @@ import java.util.Map;
  * optional.
  *
  * @author Santiago Pericas-Geertsen
+ * @author Christian Kaltepoth
  * @see javax.inject.Named
  * @see javax.enterprise.context.RequestScoped
  * @since 1.0
  */
-public interface Models extends Map<String, Object>, Iterable<String> {
+public interface Models extends Iterable<String> {
+
+    /**
+     * Stores a new model in the map.
+     *
+     * @param name  name of the model
+     * @param model model to store in the map
+     * @return the current instance to allow method chaining
+     */
+    Models put(String name, Object model);
+
+    /**
+     * Retrieve a model by name.
+     *
+     * @param name name of the model
+     * @return the model or <code>null</code>
+     */
+    Object get(String name);
+
+    /**
+     * Retrieve a model by name in a type-safe way.
+     *
+     * @param name  name of the model
+     * @param clazz type of the model
+     * @param <T>   type of the model
+     * @return The model or <code>null</code>
+     */
+    <T> T get(String name, Class<T> clazz);
+
+    /**
+     * Returns a unmodifiable view of the models map.
+     *
+     * @return unmodifiable map
+     */
+    Map<String, Object> asMap();
+
 }
